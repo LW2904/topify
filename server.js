a071497031368c5ce5dadf40660d62e22a0452c2
fsoc <8392143a@gmail.com>
2018-02-18 18:24:54 +0100

    Allow local development.

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ require('dotenv').config();
 
 const app = new (require('koa'))();
 
+app.use(async (ctx, next) => {
+  if (ctx.request.url.includes('/topify')) {
+    ctx.redirect(ctx.request.url.replace('/topify', ''));
+  } else await next();
+});
+
 app.use(require('koa-static')('public'));
 
 const server = app.listen(process.env.PORT || '8080');
