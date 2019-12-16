739d6eb3cee383db9f2d0c1d254b05716f9dac75
Laurenz Weixlbaumer <root@fsoc.space>
2019-12-16 21:00:54 +0100

    Add and use styled-jsx dependency

---
+++ preact.config.js
@@ -0,0 +1,6 @@
+export default (config, env, helpers) => {
+    const { rule } = helpers.getLoadersByName(config, 'babel-loader')[0];
+    const babelConfig = rule.options;
+
+    babelConfig.plugins.push(require.resolve('styled-jsx/babel'));
+};
